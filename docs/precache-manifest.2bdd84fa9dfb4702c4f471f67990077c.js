de91115c2ee45245570be4e37b5ba46b1c937425
Oscar Jorge <oscar.jorge.jorge@hotmail.com>
2020-08-26 14:15:37 +0200

    added docs folder

---
+++ docs/precache-manifest.2bdd84fa9dfb4702c4f471f67990077c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70059dd552db447f79d46adee0653f63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec84d77ae3f99211700d",
+    "url": "/static/css/main.4b0e29d4.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec84d77ae3f99211700d",
+    "url": "/static/js/main.7cd19a9e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
